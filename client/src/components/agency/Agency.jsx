d21147ac3419d18bc1ad4937d34651f149d8ddc2
Jeremiah Elegba <64599754+itz-jherie@users.noreply.github.com>
2023-03-22 19:24:44 +0100

    homepage almost done

--- client/src/components/agency/Agency.jsx
+++ client/src/components/agency/Agency.jsx
@@ -1,24 +1,24 @@
 import './agency.css'
 import img2 from '../../assets/web1.jpg'
-
+import CountUp from 'react-countup';
 
 const Agency = () => {
   return (
     <div className='agency'>
-      <div className='left'>
+      <div className='agency-left'>
         <img src={img2} alt='Agency' className='imgAgency'/>
       </div>
       
-      <div className='right'>
+      <div className='agency-right'>
         <h1 className='agencyhead'>Why our Agency?</h1>
         <p className='descAgency'>
         As the most popular JavaScript library for building modern, interactive user interfaces, React is an in-demand framework that'll bring real value to your career or next project. But like any technology, learning React can be tricky, and finding the right teacher can make things a whole lot easier.Maximilian Schwarzmüller is a bestselling instructor who has helped over two million students worldwide learn how to code, and his latest React video course (React—The Complete Guide) has over six hundred thousand students on Udemy.
         </p>
 
         <div  className='countContainer'>
-        <span className='count'></span>
-        <span className='count'></span>
-        <span className='count'></span>
+        <span className='count'> <span className='countNumber'><CountUp delay={2} end={9875} /></span>Satisfied Clients</span>
+        <span className='count'><span className='countNumber'><CountUp delay={2} end={7894} /></span>Projects Launched</span>
+        <span className='count'><span className='countNumber'><CountUp delay={2} end={65} /></span>Years Completed</span>
         </div>
 
       </div>
