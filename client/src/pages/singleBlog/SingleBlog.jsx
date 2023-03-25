25d2be439ccaa81419c7270b57d361524fec37b6
Emmanuel Omejua <omejuaifeanyi2@gmail.com>
2023-03-25 22:56:23 +0200

    completed blog, trying to fix media screen issues

--- client/src/pages/singleBlog/SingleBlog.jsx
+++ client/src/pages/singleBlog/SingleBlog.jsx
@@ -1,5 +1,4 @@
 import './singleblog.css'
-import Navbar from '../../components/navbar/Navbar'
 import Footer from '../../components/footer/Footer'
 import FooterDown from '../../components/footerDown/FooterDown'
 import BlogDetail from '../../components/blogdetail/BlogDetail'
