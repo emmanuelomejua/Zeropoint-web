a18ddf772cc5446450f752be79829d125aa40701
Emmanuel Omejua <omejuaifeanyi2@gmail.com>
2023-03-25 22:56:23 +0200

    completed blog, trying to fix media screen issues

--- client/src/components/newsletter/NewsLetter.jsx
+++ client/src/components/newsletter/NewsLetter.jsx
@@ -1,17 +1,19 @@
 import './newsLetter.css'
+import { Send } from '@mui/icons-material'
 
 const NewsLetter = () => {
   return (
     <div className='newletter'>
-      <div className='newsRight'>
-        <h1>Subscribe to our newsletter for latest update of our services/product</h1>
-      </div>
-      <div className='newsLeft'>
-        <div className='newsContainer'>
-        <input type='text' className='newsInput' placeholder='Email Address'/>
-        <button className='newsBtn'>Subscribe Now</button>
+       <div className='newsContainer'>
+        <h4 className='newsTitle'>Newsletter</h4>
+        <p className='newsDesc'>Subscribe to our newsletter for latest update of our services/products</p>
+
+        <div className='nInput'>
+          <input type='email' placeholder='Email Address' className='newsInput'/>
+          <button className='nBtn' type='submit'><Send/></button>
         </div>
-      </div>
+
+       </div>
     </div>
   )
 }
