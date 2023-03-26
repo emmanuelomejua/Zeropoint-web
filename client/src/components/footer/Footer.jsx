4cf2a33b943dd5d301c5032f6c253b8c714d149b
Jeremiah Elegba <64599754+itz-jherie@users.noreply.github.com>
2023-03-26 01:32:43 +0100

    feat: completed footer section

--- client/src/components/footer/Footer.jsx
+++ client/src/components/footer/Footer.jsx
@@ -8,33 +8,75 @@ const Footer = () => {
   return (
     <>
     <div className='footer'>
-
-      <div className='footleft'>
-
              <div className='contAll'>
                 <div className='cont'>
-                  <img src={img} alt='' className='fimage' height='50px' width='50pz'/>
+                  <img src={img} alt='' className='fimage' height='50px' width='50px'/>
                   <span>Zero Point</span>
                 </div>
-
-                 <p className='footDesc'>Lorem ipsum dolor, sit amet consectetur adipisicing elit. ?
+                <p className='footDesc'>Maecenas pellentesque placerat quam, in finibus nisl tincidunt sed. Aliquam magna augue, malesuada ut feugiat eget, cursus eget felis.
                 </p>
-
+                  <div className='ico'>
+                    <FacebookOutlined/>
+                    <Twitter/>
+                    <Instagram/>
+                </div>
               </div>
-
-              <div className='ico'>
-                <FacebookOutlined/>
-                <Twitter/>
-                <Instagram/>
+              <div className='footerLinks'>
+                  <h2>Our Links</h2>
+                  <div className='hr'>
+                    <hr style={{width: '20px',marginRight: '5px' ,marginTop: '5px',height: '5px', backgroundColor: 'white', border: 'none', borderRadius: '17px'}}></hr>
+                    <hr style={{width: '45px', height: '5px', marginTop: '5px', backgroundColor: 'white', border: 'none', borderRadius: '17px'}}></hr>
+                  </div>
+                  <ul>
+                    <li>Home</li>
+                    <li>About Us</li>
+                    <li>Services</li>
+                    <li>Team</li>
+                    <li>Blog</li>
+                  </ul>
               </div>
-
+              <div className='footerLinks'>
+                  <h2>Our Services</h2>
+                  <div className='hr'>
+                    <hr style={{width: '20px',marginRight: '5px' ,marginTop: '5px', height: '5px', backgroundColor: 'white', border: 'none', borderRadius: '17px'}}></hr>
+                    <hr style={{width: '45px', height: '5px', marginTop: '5px', backgroundColor: 'white', border: 'none', borderRadius: '17px'}}></hr>
+                  </div>
+                  <ul>
+                    <li>Strategy & Research</li>
+                    <li>Web Development</li>
+                    <li>Web Solution</li>
+                    <li>Digital Marketing</li>
+                    <li>App Design</li>
+                  </ul>
+              </div>
+              <div className='footerLinks'>
+                  <h2>Other Links</h2>
+                  <div className='hr'>
+                    <hr style={{width: '20px',marginRight: '5px' ,marginTop: '5px', height: '5px', backgroundColor: 'white', border: 'none', borderRadius: '17px'}}></hr>
+                    <hr style={{width: '45px', height: '5px', marginTop: '5px', backgroundColor: 'white', border: 'none', borderRadius: '17px'}}></hr>
+                  </div>
+                  <ul>
+                    <li>FAQ</li>
+                    <li>Portfolio</li>
+                    <li>Privacy Policy</li>
+                    <li>Terms & Conditions</li>
+                    <li>Support</li>
+                  </ul>
+              </div>
+              <div className='footerLinks'>
+                  <h2>Contact Us</h2>
+                  <div className='hr'>
+                    <hr style={{width: '20px',marginRight: '5px' ,marginTop: '5px', height: '5px', backgroundColor: 'white', border: 'none', borderRadius: '17px'}}></hr>
+                    <hr style={{width: '45px', height: '5px', marginTop: '5px', backgroundColor: 'white', border: 'none', borderRadius: '17px'}}></hr>
+                  </div>
+                  <ul>
+                    <li>+91 987-654-3210</li>
+                    <li>info@example.com</li>
+                    <li>Marmora Road Chi Minh City, Vietnam</li>
+                    
+                  </ul>
+              </div>
       </div>
-
-      <div className='footright'>
-
-      </div>
-    </div>
-   
     </>
   )
 }
--- client/src/components/specialization/Specialization.jsx
+++ client/src/components/specialization/Specialization.jsx
@@ -7,7 +7,7 @@ const Specialization = () => {
       <h2>Our Specialization</h2>
       <div className='specialization-container'>
         <div>
-          <p>Strategy & Research</p>
+          <p>SEO - Search Engine Optimization</p>
           <p>Mauris ut felis malesuada eros varius tristique a at orci. Nulla vulputate, leo sit amet rhoncus suscipit, enim ex venenatis ipsum, et porttitor.</p>
         </div>
         <div>
@@ -15,7 +15,7 @@ const Specialization = () => {
           <p>Mauris ut felis malesuada eros varius tristique a at orci. Nulla vulputate, leo sit amet rhoncus suscipit, enim ex venenatis ipsum, et porttitor.</p>
         </div>
         <div>
-          <p>Web Solution</p>
+          <p>Digital Marketing</p>
           <p>Mauris ut felis malesuada eros varius tristique a at orci. Nulla vulputate, leo sit amet rhoncus suscipit, enim ex venenatis ipsum, et porttitor.</p>
         </div>
       </div>
