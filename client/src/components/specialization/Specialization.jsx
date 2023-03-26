d14178aec5e40ce674d01653ee88453feb11652e
Emmanuel Omejua <omejuaifeanyi2@gmail.com>
2023-03-26 16:20:30 +0300

    all

--- client/src/components/specialization/Specialization.jsx
+++ client/src/components/specialization/Specialization.jsx
@@ -5,12 +5,11 @@ const Specialization = () => {
   return (
     <div className='specialization'>
       <h2>Our Specialization</h2>
-<<<<<<< HEAD
       <div className='sContaineer'>
         <div className='items'>
           <img src='' alt='' className='sImage'/>
           <p className='pDesc'></p>
-=======
+        </div>
       <div className='specialization-container'>
         <div>
           <p>SEO - Search Engine Optimization</p>
@@ -23,12 +22,12 @@ const Specialization = () => {
         <div>
           <p>Digital Marketing</p>
           <p>Mauris ut felis malesuada eros varius tristique a at orci. Nulla vulputate, leo sit amet rhoncus suscipit, enim ex venenatis ipsum, et porttitor.</p>
->>>>>>> 4cf2a33b943dd5d301c5032f6c253b8c714d149b
         </div>
       </div>
       <div></div>
       <div></div>
       <div></div>
+    </div>
     </div>
   )
 }
