a032d1490af3bc5d1b44ec2ff1ca31e0f668a2f8
Jeremiah Elegba <64599754+itz-jherie@users.noreply.github.com>
2023-03-28 12:09:08 +0100

    feat: added test paystack

--- client/src/components/plansubscribe/Subscribe.jsx
+++ client/src/components/plansubscribe/Subscribe.jsx
@@ -1,6 +1,42 @@
 import './subscribe.css'
-
+import { usePaystackPayment, PaystackButton } from 'react-paystack'
+import { useState } from 'react'
 const Subscribe = () => {
+
+  const publicKey = 'pk_test_ab15a6ac93a88450dfc08e30452fb84ea95c85b3'
+  const amount = 1000000
+  const [email, setEmail] = useState('jherieelegba@gmail.com')
+  const [name, setName] = useState('jherie')
+  const [phone, setPhone] = useState('+2348116619663')
+  const componentProps = {
+    email,
+    amount,
+    name,
+    phone,
+    publicKey,
+    // metadata: {
+    //   name,
+    //   phone,
+    // },
+    
+    // text: 'Pay Now',
+    // onSuccess: () => 
+    //   alert("Thanks")
+    // ,
+    // onClose: () => alert('oops!')
+  }
+  const [config, setConfig] = useState({
+		name: "jherie",
+		email: "jherieelegba@gmail.com",
+		amount: 100000,
+		publicKey: "pk_test_ab15a6ac93a88450dfc08e30452fb84ea95c85b3",
+	});
+  const initializePayment = usePaystackPayment(config)
+ const onSuccess = () => {alert('Thanks')}
+ const onClose = () => {alert('oops')}
+ 
+
+
   return (
     <div className='subscribe-container'>
       <div className='plan-detail'>
@@ -14,7 +50,7 @@ const Subscribe = () => {
           <li>SEO Marketing</li>
           <li>Business Analysis</li>
         </ul>
-        <button className='btn'>Start Now</button>
+        <button className='btn' onClick={() =>initializePayment()}>Start Now</button>
       </div>
       <div className='plan-detail'>
         <h2>Standard Plan</h2>
