66bd020c1f5950e4103d5999b74c4b5ad07d843d
Jeremiah Elegba <64599754+itz-jherie@users.noreply.github.com>
2023-03-29 23:20:25 +0100

    feat: added paystack payment

--- client/src/components/plansubscribe/Subscribe.jsx
+++ client/src/components/plansubscribe/Subscribe.jsx
@@ -2,40 +2,75 @@ import './subscribe.css'
 import { usePaystackPayment, PaystackButton } from 'react-paystack'
 import { useState } from 'react'
 const Subscribe = () => {
+  // const publicKey = 'pk_test_ab15a6ac93a88450dfc08e30452fb84ea95c85b3'
+  // const amount = 1000000
+  // const [email, setEmail] = useState('jherieelegba@gmail.com')
+  // const [name, setName] = useState('jherie')
+  // const [phone, setPhone] = useState('+2348116619663')
+  const [basicModal, setBasicModal] = useState(false)
+  const [standardModal, setStandardModal] = useState(false)
+  const [premiumModal, setPremiumModal] = useState(false)
 
-  const publicKey = 'pk_test_ab15a6ac93a88450dfc08e30452fb84ea95c85b3'
-  const amount = 1000000
-  const [email, setEmail] = useState('jherieelegba@gmail.com')
-  const [name, setName] = useState('jherie')
-  const [phone, setPhone] = useState('+2348116619663')
-  const componentProps = {
-    email,
-    amount,
-    name,
-    phone,
-    publicKey,
-    // metadata: {
-    //   name,
-    //   phone,
-    // },
-    
-    // text: 'Pay Now',
-    // onSuccess: () => 
-    //   alert("Thanks")
-    // ,
-    // onClose: () => alert('oops!')
-  }
-  const [config, setConfig] = useState({
+  /****for basic config */
+  const [basicConfig, setBasicConfig] = useState({
 		name: "jherie",
 		email: "jherieelegba@gmail.com",
 		amount: 100000,
 		publicKey: "pk_test_ab15a6ac93a88450dfc08e30452fb84ea95c85b3",
 	});
-  const initializePayment = usePaystackPayment(config)
- const onSuccess = () => {alert('Thanks')}
- const onClose = () => {alert('oops')}
- 
+/****for standard config */
+  const [standardConfig, setStandardConfig] = useState({
+		name: "jherie",
+		email: "jherieelegba@gmail.com",
+		amount: 200000,
+		publicKey: "pk_test_ab15a6ac93a88450dfc08e30452fb84ea95c85b3",
+	});
+  /****for premium config */
+  const [premiumConfig, setPremiumConfig] = useState({
+		name: "jherie",
+		email: "jherieelegba@gmail.com",
+		amount: 300000,
+		publicKey: "pk_test_ab15a6ac93a88450dfc08e30452fb84ea95c85b3",
+	});
 
+
+  const initializeBasicPayment = usePaystackPayment(basicConfig)
+  const initializeStandardPayment = usePaystackPayment(standardConfig)
+  const initializePremiumPayment = usePaystackPayment(premiumConfig)
+  const onSuccess = () => {setBasicModal(false) || setStandardModal(false) || setPremiumModal(false)}
+  const onClose = () => {setBasicModal(false) || setStandardModal(false) || setPremiumModal(false)}
+ 
+  const handleBasicChange = (e) => {
+		const valueName = e.target.name;
+		const value = e.target.value;
+		setBasicConfig({ ...basicConfig, [valueName]: value });
+		console.log(basicConfig);
+	};
+  const handleStandardChange = (e) => {
+		const valueName = e.target.name;
+		const value = e.target.value;
+		setStandardConfig({ ...standardConfig, [valueName]: value });
+		console.log(standardConfig);
+	};
+  const handlePremiumChange = (e) => {
+		const valueName = e.target.name;
+		const value = e.target.value;
+		setPremiumConfig({ ...premiumConfig, [valueName]: value });
+		console.log(premiumConfig);
+	};
+
+  const handleBasicSubmit =(e)=> {
+    e.preventDefault();
+    initializeBasicPayment(onSuccess, onClose);
+  }
+  const handleStandardSubmit =(e)=> {
+    e.preventDefault();
+    initializeStandardPayment(onSuccess, onClose);
+  }
+  const handlePremiumSubmit =(e)=> {
+    e.preventDefault();
+    initializePremiumPayment(onSuccess, onClose);
+  }
 
   return (
     <div className='subscribe-container'>
@@ -50,7 +85,22 @@ const Subscribe = () => {
           <li>SEO Marketing</li>
           <li>Business Analysis</li>
         </ul>
-        <button className='btn' onClick={() =>initializePayment()}>Start Now</button>
+        <button className='btn' onClick={()=>setBasicModal(true)}>Start Now</button>
+        {basicModal && 
+          <div className='modal-container'>
+            <div className='modal' >
+                <form onSubmit={handleBasicSubmit}>
+                  <input type="text" name='name'placeholder='Enter your Name' onChange={handleBasicChange} required/>
+                  <input type="mail" name='email'placeholder='Enter your email'onChange={handleBasicChange} required/>
+                  <input type="phone" name='phone'placeholder='Enter your Phone Number'onChange={handleBasicChange} required/>
+                  <div>
+                    <button type='submit'>Submit</button>
+                    <button onClick={() => setBasicModal(false)}>Close</button>  
+                  </div>
+                </form>
+            </div>
+          </div>
+        }
       </div>
       <div className='plan-detail'>
         <h2>Standard Plan</h2>
@@ -63,7 +113,23 @@ const Subscribe = () => {
           <li>SEO Marketing</li>
           <li>Business Analysis</li>
         </ul>
-        <button className='btn'>Start Now</button>
+        <button className='btn'onClick={()=>setStandardModal(true)}>Start Now</button>
+        {standardModal && 
+          <div className='modal-container'>
+            <div className='modal' >
+                <form onSubmit={handleStandardSubmit} >
+                  <input type="text" name='name'placeholder='Enter your Name' onChange={handleStandardChange} required/>
+                  <input type="mail" name='email'placeholder='Enter your email'onChange={handleStandardChange} required/>
+                  <input type="phone" name='phone'placeholder='Enter your Phone Number'onChange={handleStandardChange} required/>
+                  <div>
+                    <button onClick={() => initializeStandardPayment(onSuccess, onClose)}>Submit</button>
+                    <button onClick={() => setStandardModal(false)}>Close</button>  
+                  </div>
+                </form>
+                
+            </div>
+          </div>
+        }
       </div>
       <div className='plan-detail'>
         <h2>Premium Plan</h2>
@@ -76,7 +142,23 @@ const Subscribe = () => {
           <li>SEO Marketing</li>
           <li>Business Analysis</li>
         </ul>
-        <button className='btn'>Start Now</button>
+        <button className='btn'onClick={()=>setPremiumModal(true)}>Start Now</button>
+        {premiumModal && 
+          <div className='modal-container'>
+            <div className='modal' >
+                <form onSubmit={handlePremiumSubmit} >
+                  <input type="text" name='name'placeholder='Enter your Name' onChange={handlePremiumChange} required/>
+                  <input type="mail" name='email'placeholder='Enter your email'onChange={handlePremiumChange} required/>
+                  <input type="phone" name='phone'placeholder='Enter your Phone Number'onChange={handlePremiumChange} required/>
+                  <div>
+                    <button onClick={() => initializePremiumPayment(onSuccess, onClose)}>Submit</button>
+                    <button onClick={() => setPremiumModal(false)}>Close</button>  
+                  </div>
+                </form>
+                
+            </div>
+          </div>
+        }
       </div>
     </div>
   )
